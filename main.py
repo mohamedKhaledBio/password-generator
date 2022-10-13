6dd13d99f7da489b493d62ff0259cd3c34b4d8c5
Mohamed Khaled <92647105+mohamedKhaledBio@users.noreply.github.com>
2022-10-13 03:40:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for char in range(1, nr_symbols + 1):
 
 for char in range(1, nr_numbers + 1):
   password_list += random.choice(numbers)
-
+random.shuffle(password_list)
 password = ""
 for char in password_list:
   password += char
